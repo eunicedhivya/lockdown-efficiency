e74a4d410cd11f79677a1a909489b939c0f4c938
Eunice Dhivya <divi.seed@gmail.com>
2020-06-01 22:36:48 +0530

    Add axis transition (Fade in)

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -1,54 +1,52 @@
 function drawChart(selector, grpdata, linedata){
 
-    // set the dimensions and margins of the graph
-var margin = {top: 10, right: 60, bottom: 50, left: 70},
-width = 550 - margin.left - margin.right,
-height = 400 - margin.top - margin.bottom;
+  // set the dimensions and margins of the graph
+  var margin = {top: 10, right: 60, bottom: 50, left: 70},
+  width = 550 - margin.left - margin.right,
+  height = 350 - margin.top - margin.bottom;
 
-var formatDecimalComma = d3.format(",.2f")
-var formatComma = d3.format(",")
+  var subgroups = ["No. of Confirmed Cases","No. of Deaths","No. of Tests"]
 
-d3.select(selector).html(null)
-// append the svg object to the body of the page
-var svg = d3.select(selector)
-.append("svg")
-.attr("viewBox", "0 0 " + (width + margin.left + margin.right) + " " + (height + margin.top + margin.bottom))
-.attr("preserveAspectRatio", "xMinYMin")
-.append("g")
-.attr("transform",
-      "translate(" + margin.left + "," + margin.top + ")");
+  // List of groups = species here = value of the first column called group -> I show them on the X axis
+  var groups = ["Lock Down 1.0", "Lock Down 2.0", "Lock Down 3.0", "Lock Down 4.0"]
 
-var subgroups = ["No. of Confirmed Cases","No. of Deaths","No. of Tests"]
+  //Format
+  var formatDecimalComma = d3.format(",.2f")
+  var formatComma = d3.format(",")
 
-var replaceCat = {
-  "No. of Confirmed Cases": "No. of confirmed cases",
-  "No. of Deaths": "No. of deaths",
-  "No. of Tests": "No. of tests"
-}
+  //Clear the HTML Container
+  d3.select(selector).html(null)
+  
+  // append the svg object to the body of the page
+  var svg = d3.select(selector)
+  .append("svg")
+  .attr("viewBox", "0 0 " + (width + margin.left + margin.right) + " " + (height + margin.top + margin.bottom))
+  .attr("preserveAspectRatio", "xMinYMin")
+  .append("g")
+  .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
 
-// List of groups = species here = value of the first column called group -> I show them on the X axis
-var groups = ["Lock Down 1.0", "Lock Down 2.0", "Lock Down 3.0", "Lock Down 4.0"]
+  // Max No for left axis
+  var maxno = d3.max(grpdata, function(dd){
+    return dd["No. of Tests"];
+  })
 
-var maxno = d3.max(grpdata, function(dd){
-// console.log("dd", dd["No of Tests"]);
-return dd["No. of Tests"];
-})
+  // Max No for Right axis
+  var linemaxno = d3.max(linedata, function(dd){
+    return dd["lpr"];
+  })
 
-var linemaxno = d3.max(linedata, function(dd){
-// console.log("dd", dd["No of Tests"]);
-return dd["lpr"];
-})
 
-// console.log("maxno", maxno);
-
-var tool_tipline = d3.tip()
+  // Tooltip for Line Chart
+  var tool_tipline = d3.tip()
     .attr("class", "d3-tip")
     .offset([-20, 0])
     .html(function(d){
         return "<b>" + d.group + "</b><br>LPR: "+ formatDecimalComma(d.lpr);
     });
     svg.call(tool_tipline);
-
+  
+  //Tooltip for Bar Chart
   var tool_tipbar = d3.tip()
     .attr("class", "d3-tip")
     .offset([-20, 0])
@@ -59,104 +57,85 @@ var tool_tipline = d3.tip()
     svg.call(tool_tipbar);
 
 
-// Add X axis
-var x = d3.scaleBand()
-  .domain(groups)
-  .range([0, width])
-  .padding([0.2])
+  // x Axis that handles the Group
+  var x = d3.scaleBand()
+    .domain(groups)
+    .range([0, width])
+    .padding([0.2])
 
-svg.append("g")
-.attr("transform", "translate(0," + height + ")")
-.call(d3.axisBottom(x).tickSize(10))
-.attr("class", "axis xAxis");
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .transition().duration(1000)
+    .call(d3.axisBottom(x).tickSize(10))
+    .attr("class", "axis xAxis");
 
-// Add Y axis
+// y Axis that handles left axis values
 var y = d3.scaleLinear()
-.domain([0, maxno])
-.range([ height, 0 ]);
-svg.append("g")
-.call(d3.axisLeft(y))
-.attr("class", "axis");
+  .domain([0, maxno])
+  .range([ height, 0 ]);
 
-// Another scale for subgroup position?
+svg.append("g")
+  .transition().duration(1000)
+  .call(d3.axisLeft(y))
+  .attr("class", "axis");
+
+// Scale that handles the subgroup
 var xSubgroup = d3.scaleBand()
-.domain(subgroups)
-.range([0, x.bandwidth()])
-.padding([0.05])
+  .domain(subgroups)
+  .range([0, x.bandwidth()])
+  .padding([0.05])
 
 // color palette = one color per subgroup
 var color = d3.scaleOrdinal()
-.domain(subgroups)
-.range(['#D55D29','#6CA44B','#3B7AC9'])
+  .domain(subgroups)
+  .range(['#D55D29','#6CA44B','#3B7AC9'])
 
 svg.selectAll(".groupbg")
-    .data(grpdata).enter()
-    .append("rect")
-    .attr("class", "groupbg")
-    .attr("width", "80px")
-    .attr("fill", "#e8e8e8")
-    .attr("x", function(D, i){
-      return x(D["group"])
-    })
-    .attr("y", function(D){ return 0 })
-    .attr("height", function(D){ return height})
+  .data(grpdata).enter()
+  .append("rect")
+  .attr("class", "groupbg")
+  .attr("width", "80px")
+  .attr("fill", "#e8e8e8")
+  .attr("x", function(D, i){
+    return x(D["group"])
+  })
+  .attr("y", function(D){ return 0 })
+  .attr("height", function(D){ return height})
 
 
-// svg.selectAll(".groupbg")
-//     .data(grpdata).enter()
-//     .append("rect")
-//     .attr("class", "groupbg")
-//     .attr("width", "80px")
-//     .attr("fill", "#e8e8e8")
-//     .attr("x", function(D, i){
-//       // console.log(D["No. of Tests"]);
-//       return x(D["group"])
-//     })
-//     .attr("y", function(D){ return y(0) })
-//     .attr("height", function(D){ return height - y(0) })
-
-// svg.selectAll(".groupbg")
-//     .transition().duration(1000)
-//     .attr("y", function(D){
-//       return y(D["No. of Tests"])
-//     })
-//     .attr("height", function(D){
-//       return height - y(D["No. of Tests"])
-//     })
 
 // Show the bars
 svg.append("g")
-.selectAll("g")
-// Enter in data = loop group per group
-.data(grpdata)
-.enter()
-.append("g")
-  .attr("class", "lockdown-group")
-  .attr("transform", function(d) { return "translate(" + x(d.group) + ",0)"; })
-.selectAll(".bar")
-.data(function(d) { 
-  var pd = subgroups.map(function(key) { 
-    // console.log("key", key);
-    // console.log("d",  d[key]);
-    
-    return {key: key, value: parseInt(d[key])}; 
+  .selectAll("g")
+  .data(grpdata)
+  .enter()
+  .append("g")
+    .attr("class", "lockdown-group")
+    .attr("transform", function(d) { return "translate(" + x(d.group) + ",0)"; })
+  .selectAll(".bar")
+  .data(function(d) { 
+    var pd = subgroups.map(function(key) { 
+      // console.log("key", key);
+      // console.log("d",  d[key]);
+      
+      return {key: key, value: parseInt(d[key])}; 
+    })
+    return pd; 
   })
-  return pd; 
-})
-.enter().append("rect")
-    .attr("class", "bar")
-    .attr("x", function(d) { return xSubgroup(d.key); })
-    .attr("y", function(d) { return y(0); })
-    .attr("width", xSubgroup.bandwidth())
-    .attr("height", function(d) { return height - y(0); })
-    .on('mouseover', tool_tipbar.show)
-    .on('mouseout', tool_tipbar.hide)
-    
-    svg.selectAll(".bar")
-    .transition().duration(1000)
-    .attr("y", function(d) { return y(d.value); })
-    .attr("height", function(d) { return height - y(d.value); })
-    .attr("fill", function(d) { return color(d.key); })
+  .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return xSubgroup(d.key); })
+      .attr("y", function(d) { return y(0); })
+      .attr("width", xSubgroup.bandwidth())
+      .attr("height", function(d) { return height - y(0); })
+      .on('mouseover', tool_tipbar.show)
+      .on('mouseout', tool_tipbar.hide)
+      
+svg.selectAll(".bar")
+  .transition().duration(1000)
+  .attr("y", function(d) { return y(d.value); })
+  .attr("height", function(d) { return height - y(d.value); })
+  .attr("fill", function(d) { return color(d.key); })
 
 svg.append("g")
   .selectAll("g")
@@ -223,8 +202,6 @@ var totalLength = path.node().getTotalLength();
     .duration(1000) // Set Duration timing (ms)
     .ease(d3.easeLinear) // Set Easing option
     .attr("stroke-dashoffset", 0); // Set final value of dash-offset for transition
-
-  // console.log("line", line(data2));
   
 
 svg.selectAll(".dot")
